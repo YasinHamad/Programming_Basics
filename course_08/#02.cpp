3d70deb2e58ee830c04a0114ccdefa92717b74cc
YasinHamad <yasin.hamad27@gmail.com>
2026-01-28 22:40:19 +0200

    course [08] solve problem [02]

---
+++ course_08/#02.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <cmath>
+#include <string> // to use the string object
+#include <cstdlib>
+#include <vector>
+#include <cctype> // isupper(), isdigit()
+#include <iomanip> // for setw()
+//#include <fstream> // for files
+//#include <ctime> // for time
+#include <print>
+
+
+//#include <sstream> // for ostringstream oss;
+
+#include "readers.h"
+#include "printers.h"
+#include "converters.h"
+#include "files.h"
+#include "datetime.h"
+#include "generators.h"
+#include "allocators.h"
+#include "fillers.h"
+#include "manipulators.h"
+
+using namespace std;
+
+#define SIZE 50
+const string ACCOUNTS_FILE = "accounts.txt";
+/* --------------------------------- FUNCTIONS --------------------------------- */
+bool IsLeapYear(int year) {
+	if (year % 4 == 0) {
+		if (year % 100 != 0) return true;
+		if (year % 400 == 0) return true;
+	}
+	return false;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* --------------------------------- FUNCTIONS --------------------------------- */
+
+
+int main()
+{
+	while (true) {
+		srand((unsigned)time(NULL));
+		/* -------------------------------- CODE --------------------------------- */
+
+
+		/* --------------------------------- VARIABLES --------------------------------- */
+		//string test_str = readers::ReadASentence("Enter a message please: ");
+		//string test_str = "my name is actaully yasin, yasin is my first name, yes yasin";
+		//char c = readers::ReadChar("Enter your char: ");
+		//char* arr = NULL;
+		//int arr_size = 0;
+		int test_num = readers::ReadNumber("Enter a number: ");
+		/* --------------------------------- VARIABLES --------------------------------- */
+
+		/* --------------------------------- MAIN WORK --------------------------------- */
+		cout << IsLeapYear(test_num) << endl;
+
+		/* --------------------------------- MAIN WORK --------------------------------- */
+
+
+		/* --------------------------------- PRINT --------------------------------- */
+
+
+
+		/* --------------------------------- PRINT --------------------------------- */
+
+
+		/* --------------------------------- DELETE --------------------------------- */
+		/* --------------------------------- DELETE --------------------------------- */
+
+		/* --------------------------------- CODE --------------------------------- */
+		//break;
+	}
+
+	return 0;
+}
